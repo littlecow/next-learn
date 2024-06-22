1489b3cc10322fd89e9294464491e9de5b2a53d1
Les chao <leschao@gmail.com>
2024-06-22 17:28:31 -0400

    Fixed issue
    Removed createPageURL string.

--- dashboard/starter-example/app/ui/invoices/pagination.tsx
+++ dashboard/starter-example/app/ui/invoices/pagination.tsx
@@ -26,7 +26,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
       { <div className="inline-flex">
         <PaginationArrow
           direction="left"
-          href={createPageURL(currentPage - 1)}createPageURL
+          href={createPageURL(currentPage - 1)}
           isDisabled={currentPage <= 1}
         />
 
